a64a6d8e6edf8f9d99e7e0ead13573c63e7f6ca3
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-27 12:02:58 +0000

    array + roupe

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 {
-	// const score1 = 80;
-	// const score2 = 90;
-	// const score3 = 40;
+
 	const scores = [80,90,40]
-	console.log(scores);
+
+
+	//同じ役割　i インデックス
+	for (let i = 0; i < 3; i ++){
+		console.log(`Scores: ${scores[i]}`);
+	}
+
+    for (let i = 0; i < scores.length; i++) {
+  		console.log(`Score ${i}: ${scores[i]}`);
+  	}
 }
+
