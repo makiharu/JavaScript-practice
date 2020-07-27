5f00f668ea84bd602d194d8c38f08cb43034fd54
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-27 12:28:30 +0000

    [add]splice

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 'use strict';
 
 {
-
 	const scores = [80,90,40]
-	scores.push(60, 50);
-	scores.shift();
-	scores.unshift(10, 20);
-	scores.pop();
 
-	//10 20 90 40 60
+	scores.splice(1,1, 30,60,90);
+	//80 30 60 90 40
 
-    for (let i = 0; i < scores.length; i++) {
-  		console.log(`Score ${i}: ${scores[i]}`);
-  	}
+
+	for (let i = 0; i < scores.length; i ++) {
+		console.log(`Score ${i}: ${scores[i]}`);
+	}
 }
 
+//splice(変化が開始する位置, 削除数, 追加する要素)
