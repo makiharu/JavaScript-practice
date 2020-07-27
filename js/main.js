347883aeb2ee87ee8693c8bcb4db72038f2e947d
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-27 14:06:45 +0000

    [finish]obj-10

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 'use strict';
 
-{
-	const prices = [180, 190, 200];
+// {
+// 	//偶数の要素だけ配列として抽出する方法は
+// 	const numbers = [1, 4, 7,8,10];
 
-	const updatePrices = prices.map(price => price + 20);
-	console.log(updatePrices);
+// 	//その１
+// 	// const evenNumbers = numbers.filter(number => {
+// 	// 	if (number % 2 === 0) {
+// 	// 		return true;
+// 	// 	} else {
+// 	// 		return false;
+// 	// 	}
+// 	// });
 
-}
+// 	// console.log(evenNumbers);
 
+// 	//１よりも簡単に
+// 	const evenNumbers = numbers.filter(number => number % 2 === 0);
+// 	console.log(evenNumbers);
+// }
+
+
+//オブジェクト記法
+
+//const ponit = [100, 180];
+
+const point = {x: 100, y: 180};
+console.log(ponit);
