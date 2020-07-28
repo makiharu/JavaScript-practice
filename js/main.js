3b197d798e463d36bb5724bfad4bb854e9a2e755
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-28 09:13:49 +0000

    [finish]obj-23

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 'use strict';
 
 {
-  //console.log(Math.random());
+	//alert('hello');
 
-  //0,1,2
-  // Math.fllor(Math.random() * 3);
-
-  // //0,...,n
-  // Math.floor(Math.random() * (n + 1));
-
-  // //min,...max
-  // Math.floor(Math.random() * (max + 1 - min)) + min
-
-  console.log(Math.floor(Math.random() * 6) + 1);
-
+	const answer = confirm('削除しますか？');
+	if (answer) {
+		console.log('削除しますか？');
+	} else {
+		console.log('キャンセルしました');
+	}
 }
