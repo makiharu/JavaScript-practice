b53c4d4a9ec34ba205ac1067498bb3fafb45aa56
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-28 07:20:37 +0000

    [Finish]obj-19

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 'use strict';
 
 {
-	//join()
-	//1 /区切りの方法
-	//2 区切りなし表示
-	const d = [2020, 7, 28];
-	console.log(d.join('/')); //1 2020/7/28
-	console.log(d.join('')); //2 2020728
+  const scores = [10, 3, 9];
 
-	//split()
-	const t = '17:08:24';
-	console.log(t.split(':'));
+  let sum = 0;
+  scores.forEach((score) => {
+  	sum += score;
 
-	const [hour, minute, second] = t.split(':');
-	console.log(hour);
-	console.log(minute);
-	console.log(second);
- }
+  });
+
+  const avg = sum / scores.length;
+
+  // console.log(sum);
+  // console.log(avg);
+
+  console.log(Math.floor(avg)); //7
+  console.log(Math.ceil(avg));//8
+  console.log(Math.round(avg));//7
+  console.log(avg.toFixed(3));//7.33333
+
+  console.log(Math.random());
+
+}
 
 
