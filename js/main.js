a8087ae8029aaa50df077f99db2bd8a897547a73
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-28 09:52:00 +0000

    [finish]obj-25

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 'use strict';
 
-//タイマー機能について
-//現在日時を表示する関数があったとして、それを 1 秒ごとにくり返す
-//setInterval() という命令を使うのですが、このように関数とミリ秒単位の数値を渡すと、
-//この関数をこちらのミリ秒間隔でくり返し実行してくれます。
-
-
 {
 	let i = 0;
-
 	function showTime() {
 		console.log(new Date());
+		const timeoutID = setTimeout(showTime, 1000);
 		i ++;
 		if (i > 2) {
-			clearInterval(intervalId);
+			clearTimeout(timeoutID);
 		}
 	}
 
-	const intervalId = setInterval(showTime, 1000);
+	showTime();
 }
 
