8622efd8100b87f8bb5debbb52dcd5a737cba9d6
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 10:13:59 +0000

    [Finish]dom-10

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 'use strinct';
-
+// li要素を追加する
 {
   document.querySelector('button').addEventListener('click', () => {
-  	const targetNode = document.getElementById('target');
+  	//要素を追加するが、これだけだと空の要素ができただけ
+  	const item2 = document.createElement('li');
+  	//テキストの中身を記述
+  	item2.textContent = 'item 2';
 
-    // targetNode.className = 'my-color';
-    // targetNode.className = 'my-color mey-border';
-    // targetNode.classList.add('my-color');
-
-    // no.1
-    // if (targetNode.classList.contains('my-color') === true) {
-    // 	targetNode.classList.remove('my-color');
-    // } else {
-    // 	targetNode.classList.add('my-color');
-    // }
-
-    //no.2
-   targetNode.classList.toggle('my-color');
-
-
-
+  	//DOM ツリーに追加すれば良いが、まずは親要素である ul 要素を取得
+  	const ulNode = document.querySelector('ul');
+  	ulNode.appendChild(item2);
   });
 }
 
 
+// 'use strict';
+
+// {
+//   document.querySelector('button').addEventListener('click', () => {
+//     const item2 = document.createElement('li');
+//     item2.textContent = 'item 2';
+
+//     // const ulNode = document.querySelector('ul');
+//     const ul = document.querySelector('ul');
+//     ul.appendChild(item2);
+//   });
+// }
