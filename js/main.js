59bdd2c051e2b4b1202d03ebaca72b3b892da7c7
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 14:16:03 +0000

    [Finish]1-20

--- js/main.js
+++ js/main.js
@@ -1,29 +1,12 @@
 'use strinct';
 
 
-{
-  const text = document.querySelector('textarea');
-
-  // text.addEventListener('focus', () => {
-  //   console.log('focus');
-  // });
-
-  // text.addEventListener('blur', () => {
-  //   console.log('blur');
-  // });
-
-  //input 内容が更新されたとき
-  text.addEventListener('input', () => {
-  	// console.log('input');
-  	console.log(text.value.length)
-  });
-  //change　更新が確定したとき
-  text.addEventListener('change', () => {
-  	console.log('change');
-  });
-
-  //リロードして文字を入力するたびに input イベントが呼ばれていて、
-  //入力をおえてフォーカスを外してあげると change イベントがちゃんと呼ばれています。
+{	//フォームを送信するとページ遷移が発生するため、一瞬だけ表示されたあとにページがリロードされてしまう
+	//Eventオブジェクトを渡してあげて、、既定の動作をキャンセルすることができる preventDefault() というメソッドを実行
+	document.querySelector('form').addEventListener('submit', (e) => {
+		e.preventDefault();
+		console.log('submit');
+	});
 
 }
 
