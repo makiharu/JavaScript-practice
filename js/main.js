f18fe4626938f041ee57f5d6b9635e86425448ad
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 12:43:07 +0000

    [Finish]dom-114

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strinct';
-// domから要素を削除する
+// セレクトボックス(赤、青、黄)
 {
   document.querySelector('button').addEventListener('click', () => {
   	const li = document.createElement('li');
-  	const text = document.querySelector('input');
-  	li.textContent = text.value;
+  	const color = document.querySelector('select');
+  	//li の textContent に値をセット
+  	//select の場合 value プロパティで選択された値、
+  	//そして selectedIndex プロパティで選択された値が何番目かを取得できる
+  	li.textContent = `${color.value} - ${color.selectedIndex}`;
   	document.querySelector('ul').appendChild(li);
-
-  	text.value = '';
-  	text.focus();
   });
 }
 
