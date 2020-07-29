a325b44185c38b3ac4a48ee2a8d81240812b7c0e
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 06:33:22 +0000

    [FInish]obj-32

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 'use strict';
 
 {
-	//ユーザーから文字を受け取って、大文字にする
-	//文字型ではなく、数値が値として渡った場合の処理方法について
+	class Post {
+		constructor(text) {
+			this.text = text,
+			this.likeCount = 0;
+		}
 
-	const name = '5';
-
-	try {
-		console.log(name.toUpperCase());
-	} catch (e) {
-		console.log(e);
+		//プロパティがメソッドのとき、functionを省略できる
+		show() {
+			//同じオブジェクト内のプロパティにアクセスするには、thisを使う
+			console.log(`${this.text} - ${this.likeCount}いいね`)
+		}
 	}
 
 
-	console.log('Finish');
+	const posts = [
+		new Post('JacaScriptの勉強'),
+		new Post('プログラミング学習')
+	];
+
+	//show(posts[0]);
+	posts[1].show();
+	posts[0].show();
 }
-
-
-
