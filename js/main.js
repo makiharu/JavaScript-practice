6162f981d6b8c5d7726d6a84c0b8f9d85d84da30
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 07:46:53 +0000

    [Finish]dom-2

--- js/main.js
+++ js/main.js
@@ -1,48 +1,13 @@
-'use strict';
+'use strinct';
 
-{
-	class Post {//親クラス
-		constructor(text) {
-			this.text = text;
-			this.likeCount = 0;
-		}
+  {
+  	function update() {
+  		//document.querySelector('h1').textContent = 'Changed!';
 
-		//プロパティがメソッドのとき、functionを省略できる
-		show() {
-			//同じオブジェクト内のプロパティにアクセスするには、thisを使う
-			console.log(`${this.text} - ${this.likeCount}likes`)
-		}
+  		document.querySelector('#target').textContent = 'Changed!-target';
 
-		like() {
-			this.likeCount++;
-			this.show();
-		}
+  		//document.getElementById('target').textContent = 'Changed!-getById'
+  	}
 
-	}
-	//クラスの拡張
-	//SponsoredPost クラスには like() メソッドはないが、 Post クラスの like() メソッドが引き継がれる
-	class SponsoredPost extends Post{//子クラス　継承
-		constructor(text, sponsor) {
-			super(text);//親クラスのconstructorを呼ぶ
-			this.sponsor = sponsor;
-		}
-
-		show() {
-			//こちらの処理ですが、親クラスの show() メソッドを使えば良いので、その場合 super に繋げて書けば
-			//親クラスのメソッドを呼べるというルールもある。
-			super.show();
-			console.log(`... sponsored by ${this.sponsor}`);
-		}
-	}
-
-	const posts = [
-		new Post('JacaScriptの勉強'),
-		new Post('プログラミング学習'),
-		new SponsoredPost('JSを使いこなす', 'hogehoge'),
-	];
-
-	posts[2].show();//JSを使いこなす - 0
-					// ... sponsored by hogehoge
-	posts[2].like();//JSを使いこなす - 1
-					// ... sponsored by hogehoge
-}
+  	setTimeout(update, 1000);
+  }
