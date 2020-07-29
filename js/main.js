5241e56c3c30062a3fd9a127a5aa526b3db45ba7
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 13:08:18 +0000

    [Finish]dom-15

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 'use strinct';
 // セレクトボックス(赤、青、黄)
+
 {
   document.querySelector('button').addEventListener('click', () => {
-  	const li = document.createElement('li');
-  	const color = document.querySelector('select');
-  	//li の textContent に値をセット
-  	//select の場合 value プロパティで選択された値、
-  	//そして selectedIndex プロパティで選択された値が何番目かを取得できる
-  	li.textContent = `${color.value} - ${color.selectedIndex}`;
-  	document.querySelector('ul').appendChild(li);
+    const colors = document.querySelectorAll('input');
+    let selectedColor;
+
+    colors.forEach(color => {
+      if (color.checked === true) {
+        selectedColor = color.value;
+      }
+    });
+
+    const li = document.createElement('li');
+    li.textContent = selectedColor;
+    document.querySelector('ul').appendChild(li);
   });
 }
-
