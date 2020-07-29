c4cf028d730266c0b38a88dda830c020c5204ffb
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 09:41:26 +0000

    [Finish]dom-8

--- js/main.js
+++ js/main.js
@@ -4,13 +4,22 @@
   document.querySelector('button').addEventListener('click', () => {
   	const targetNode = document.getElementById('target');
 
-  	//この要素に他のクラスがなければ、これでok
-    targetNode.className = 'my-color';
+    // targetNode.className = 'my-color';
+    // targetNode.className = 'my-color mey-border';
+    // targetNode.classList.add('my-color');
 
-    //他のクラスがある場合
-    //ここで、 my-color のスタイルを付けるためにボタンをクリックすると…、border が消えてしまう
-	//なぜかというと、 className を使うとクラス属性の値が丸っと書き換わって、 my-color だけになってしまうから
-    targetNode.className = 'my-color my-border';
+    // no.1
+    // if (targetNode.classList.contains('my-color') === true) {
+    // 	targetNode.classList.remove('my-color');
+    // } else {
+    // 	targetNode.classList.add('my-color');
+    // }
+
+    //no.2
+   targetNode.classList.toggle('my-color');
+
+
+
   });
 }
 
