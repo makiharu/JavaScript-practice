72be1250136b0c4d5e683fe460c74f44386b60c2
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 06:41:42 +0000

    [Finish]obj-33

--- js/main.js
+++ js/main.js
@@ -3,14 +3,19 @@
 {
 	class Post {
 		constructor(text) {
-			this.text = text,
+			this.text = text;
 			this.likeCount = 0;
 		}
 
 		//プロパティがメソッドのとき、functionを省略できる
 		show() {
 			//同じオブジェクト内のプロパティにアクセスするには、thisを使う
-			console.log(`${this.text} - ${this.likeCount}いいね`)
+			console.log(`${this.text} - ${this.likeCount}likes`)
+		}
+
+		like() {
+			this.likeCount++;
+			this.show;
 		}
 	}
 
@@ -20,7 +25,9 @@
 		new Post('プログラミング学習')
 	];
 
+	posts[0].like();
 	//show(posts[0]);
 	posts[1].show();
 	posts[0].show();
 }
+
