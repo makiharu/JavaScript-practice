4e16c23ed4af9b2cc4ae012e529d5126a29002a8
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 08:45:36 +0000

    [Finish]dom-5

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 'use strinct';
 
-  {
-  	function update() {
-  		//document.querySelector('h1').textContent = 'Changed!';
 
-  		document.querySelector('#target').textContent = 'Changed!-target';
-
-  		//document.getElementById('target').textContent = 'Changed!-getById'
-  	}
-
-  	setTimeout(update, 1000);
-  }
+{
+  document.querySelector('button').addEventListener('click', () => {
+    document.getElementById('target').textContent = 'Changed!';
+  });
+}
