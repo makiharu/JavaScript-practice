a434af070d9906deaea77edb8347d082b310eac5
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 14:28:03 +0000

    [Finish]dom-21

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strinct';
 
 
-{	//フォームを送信するとページ遷移が発生するため、一瞬だけ表示されたあとにページがリロードされてしまう
-	//Eventオブジェクトを渡してあげて、、既定の動作をキャンセルすることができる preventDefault() というメソッドを実行
-	document.querySelector('form').addEventListener('submit', (e) => {
-		e.preventDefault();
-		console.log('submit');
-	});
-
+{
+	document.querySelector('ul').addEventListener('click', e => {
+		//これは、Element.tagNameはタグ名を大文字で返す
+	    if (e.target.nodeName === 'LI') {
+	      e.target.classList.toggle('done');
+	    }
+	  });
 }
 
