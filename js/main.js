4f5b5e3554200547fe22e0279da860d9b758e231
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 13:53:49 +0000

    [Finish]dom-18

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 'use strinct';
-// チェックボックス(赤、青、黄)
+
 
 {
-	//ダブルクリック
-	document.querySelector('button').addEventListener('dblclick', () => {
-		console.log('Double cliccked');
+	document.querySelector('button').addEventListener('dblclikc', () => {
+		console.log('double clicked');
 	});
-	//マウス移動
-	document.querySelector('button').addEventListener('mousemove', () => {
-		console.log('move');
+//この関数に引数を渡せば、ブラウザがイベントに関する情報をセットして渡してくれるという仕組み
+	// document.addEventListener('mouseover', e => {
+	// 	//consle.log('moved');
+	// 	console.log(e.clientX, e.clientY);
+	// });
+
+//文書内でキーを押したときに何のキーが押されたかを表示
+	document.addEventListener('keydown', e => {
+		console.log(e.key);
 	});
 
 }
+
