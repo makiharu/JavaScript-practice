423c10c7fca328b482220280ef753ae46bf04aad
makiharu <kitakazetoshirokuma@gmail.com>
2020-07-29 10:26:31 +0000

    [Finish]dom-12

--- js/main.js
+++ js/main.js
@@ -1,28 +1,12 @@
 'use strinct';
-// li要素を追加する
+// domから要素を削除する
 {
   document.querySelector('button').addEventListener('click', () => {
-  	//要素を追加するが、これだけだと空の要素ができただけ
-  	const item2 = document.createElement('li');
-  	//テキストの中身を記述
-  	item2.textContent = 'item 2';
+  	const item1 = document.querySelectorAll('li')[1];
 
-  	//DOM ツリーに追加すれば良いが、まずは親要素である ul 要素を取得
-  	const ulNode = document.querySelector('ul');
-  	ulNode.appendChild(item2);
+  	// item1.remove();
+
+  	//親Node.removeChild(削除するNode)
+  	document.querySelector('ul').removeChild(item1);
   });
 }
-
-
-// 'use strict';
-
-// {
-//   document.querySelector('button').addEventListener('click', () => {
-//     const item2 = document.createElement('li');
-//     item2.textContent = 'item 2';
-
-//     // const ulNode = document.querySelector('ul');
-//     const ul = document.querySelector('ul');
-//     ul.appendChild(item2);
-//   });
-// }
