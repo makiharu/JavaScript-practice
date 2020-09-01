f848f6471ed96f04dce9312de95e473705603949
github <kitakazetoshirokuma@gmail.com>
2020-09-01 17:51:02 +0900

    [Finish]signal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 'use strinct';
 
-const signal = 'red'
+const signal = 'pink'
 
+switch (signal) {
+	case 'red':
+		console.log('止まれ');
+	break;
+	case 'yellow':
+		console.log('注意して進め');
+		break;
+	case 'green':
+		console.log('進んでもいいよ');
+	break;
+	default:
+		console.log('Wrong signal!');
+	break;
+}
 
