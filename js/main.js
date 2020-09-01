889d2f02adab3d0cbadf4e57b4b8dcb26f998e9e
github <kitakazetoshirokuma@gmail.com>
2020-09-01 17:43:31 +0900

    [Finish]switch

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 'use strinct';
 
+const signal = 'red'
 
-{
-	document.querySelector('ul').addEventListener('click', e => {
-		//これは、Element.tagNameはタグ名を大文字で返す
-	    if (e.target.nodeName === 'LI') {
-	      e.target.classList.toggle('done');
-	    }
-	  });
-}
 
