f135405ed388c8fc3c160929f26b86fb08fa0ada
github <kitakazetoshirokuma@gmail.com>
2020-09-01 18:43:28 +0900

    [Finish]function

--- js/main.js
+++ js/main.js
@@ -1,25 +1,14 @@
-'use strinct';
+'use strict';
 
-// const signal = 'pink'
-
-// switch (signal) {
-// 	case 'red':
-// 		console.log('止まれ');
-// 	break;
-// 	case 'yellow':
-// 		console.log('注意して進め');
-// 		break;
-// 	case 'green':
-// 		console.log('進んでもいいよ');
-// 	break;
-// 	default:
-// 		console.log('Wrong signal!');
-// 	break;
-// }
-
-for (let i = 1; i <= 10; i ++) {
-	if (i % 3 === 0) {
-		continue;
-	}
-	console.log(i);
+function showAdd() {
+	console.log('---------');
+	console.log(' ----Ad-----');
+	console.log('---------');
 }
+
+showAdd();
+console.log('tom is great');
+showAdd();
+console.log('Bob is great');
+showAdd();
+console.log('May is great!');
