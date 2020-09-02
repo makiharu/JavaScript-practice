93a403fc04bca99c0fe0c10e7c1001ecc4d4798b
github <kitakazetoshirokuma@gmail.com>
2020-09-02 23:18:00 +0900

    [Finish]25

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 'use strict';
 
+function f() {
+	const x = 3;
+	console.log(x);
+}
 
-// function sum(a,b,c) {
-// 	return a + b + c;
-// }
-
-const sum = function(a,b,c) {
-	return a + b + c;
-};
-
-const total = sum(5,4,3) + sum(1,3,4);
-console.log(total);
+f();
