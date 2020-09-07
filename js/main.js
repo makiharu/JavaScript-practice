b46afcbb19265b22a73a1711e07c044edf5621ca
github <kitakazetoshirokuma@gmail.com>
2020-09-07 21:12:48 +0900

    [Finish]

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
-function f() {
-	const x = 3;
-	console.log(x);
-}
+{
+	//テンプレートリテラルを用いて、配列の各値を表示せよ。
+	//Score: 40
+// 	Score: 50
+//  Score: 40
+	const scores = [40, 90, 40];
+	scores.splice(1, 1, 50, 50);
+	for (let i =0; i < scores.length; i++) {
+		console.log(`Score ${i}: ${scores[i]}`);
+	}
 
-f();
+}
