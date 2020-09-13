aac9de44a6ff9eba50124e10e43ac97143402f5b
github <kitakazetoshirokuma@gmail.com>
2020-09-13 22:58:11 +0900

    Finish

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 'use strict';
-
 {
-	//テンプレートリテラルを用いて、配列の各値を表示せよ。
-	//Score: 40
-// 	Score: 50
-//  Score: 40
-	const scores = [40, 90, 40];
-	scores.splice(1, 1, 50, 50);
-	for (let i =0; i < scores.length; i++) {
-		console.log(`Score ${i}: ${scores[i]}`);
+	function update() {
+		 document.getElementById('target').textContent = 'こんにちは';
+		document.querySelectorAll('p').forEach((p, index) => {
+			p.textContent = `${index}番目のpです！`;
+		});
 	}
-
+	setTimeout(update, 1000);
 }
