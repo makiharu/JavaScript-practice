3f8c8152bb49c6e67d3d1f256dc0fa29c4f502e0
makiharu <kitakazetoshirokuma@gmail.com>
2021-02-06 22:08:58 +0900

    omikuji-finshbutton追加

--- Omikuji/main.js
+++ Omikuji/main.js
@@ -2,10 +2,22 @@
 
  {
     const btn = document.getElementById('btn');
+    const endbtn = document.getElementById('end');
+
+    //おみくじボタンを押下した場合
     btn.addEventListener('click', () => {
-        const msg = ['hoge', 'fuga', 'poit'];
+        const msg = ['ストップウォッチを15分以内に作成する', 'おみくじを作成する', '新しいことを学ぶ'];
         const n = Math.floor(Math.random() * msg.length);
-        btn.textContent = msg[n];
+    
+        if(n < 0.3) {
+            btn.textContent = msg[n];
+        } else if(n < 0.8) {
+            btn.textContent = msg[n];
+        } else {
+            btn.textContent = msg[n];
+        }
+
+        //背景の設定
         switch(n) {
             case 0:
                 btn.style.background = '#ffff99';
@@ -18,5 +30,16 @@
                 break;
         }
             });
-    
+
+        //タスクが完了した場合
+        endbtn.addEventListener('click', () => {
+            //完了メッセージを表示した後、もとの表示に戻す
+            btn.textContent = 'よくできました!!!';
+            btn.style.background = '#ef454a';
+
+            setTimeout(() => {
+                btn.textContent = 'もう一度挑戦する？';
+                btn.style.background = '#ffa500';
+            }, 2000);
+        });
  }
